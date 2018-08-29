ab8b7f32025b47bbeae83e97774d4e04a2a2bd63
Manguo1401 <aaronleehartnell@gmail.com>
2018-08-29 10:39:46 +0300

    first socket message emits and receive

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+let socket = io();
+
+socket.on('connect', function () {
+    console.log('Connected to server');
+
+    socket.emit('createMessage', {
+        to: 'louis',
+        text: 'fu'
+    })
+});
+
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from the server');
+});
+
+socket.on('newMessage', function(message) {
+    console.log('newMessage: ', message);
+});
